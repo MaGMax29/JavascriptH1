2d8c6c6e64039dc2374a806f649a8273f1224ca9
Maria Ramirez <maga.ramirez86@gmail.com>
2022-03-29 22:03:39 +0100

    adding form and algorithms

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* CREAR UN ALGORITMO CON CONDICIONAL */
 
-let maxSpeed = prompt ("max speed 120");
+/* let maxSpeed = prompt ("max speed 120");
 
 if (maxSpeed == 120){
     alert("Send a message");
@@ -9,7 +9,43 @@ if (maxSpeed == 120){
 } else{
     alert ("nothing happens");
 }
-
+ */
 
 /* CREAR UN ALGORITMO UTILIZANDO UN CICLO */
+
+/* CICLO DE CONTEO */
+
+/* let ingresarNumero = parseInt ( prompt ("Ingresar Numero")); 
+
+for (let i = 2; i <= 10; i++){
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " " + i + "=" + resultado );
+} */
+
+/* CICLOS CONDICIONALES */
+
+let data = 0;
+
+do{
+    console.log(data);
+    if (data == 1){
+        break;
+    }
+    data++;
+} while(data <=10);
+
+console.log ("End of cicle");
+
+
+/* SIMULADOR INTERACTIVO */
+
+/* PROCESO CALCULO DEL IVA */
+
+var precio = 100;
+var ivaEnFactura = 23;
+
+var iva = precio * ivaEnFactura/100;
+var precioTotal = precio + iva;
+
+alert( precioTotal);
 
