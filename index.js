12fd7c352d00058ac2219a1aa5caef268a5b0950
Maria Ramirez <maga.ramirez86@gmail.com>
2022-03-29 14:54:28 +0100

    condicional

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 /* CREAR UN ALGORITMO CON CONDICIONAL */
 
+let maxSpeed = prompt ("max speed 120");
+
+if (maxSpeed == 120){
+    alert("Send a message");
+} else if (maxSpeed == 100){
+    alert("Gives a warning");
+} else{
+    alert ("nothing happens");
+}
+
+
+/* CREAR UN ALGORITMO UTILIZANDO UN CICLO */
+
