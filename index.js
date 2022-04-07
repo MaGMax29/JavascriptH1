ab422d70742b591e9f1f2f8a520b9f773464c472
Maria Ramirez <maga.ramirez86@gmail.com>
2022-04-07 20:40:03 +0100

    agregando arrays

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* CREAR UN ALGORITMO CON CONDICIONAL */
 
-let maxSpeed = prompt ("max speed 120");
+/* let maxSpeed = prompt ("max speed 120");
 
 if (maxSpeed == 120){
     alert("Send a message");
@@ -8,23 +8,23 @@ if (maxSpeed == 120){
     alert("Gives a warning");
 } else{
     alert ("nothing happens");
-}
+} */
 
 
 /* CREAR UN ALGORITMO UTILIZANDO UN CICLO */
 
 /* CICLO DE CONTEO */
 
-let ingresarNumero = parseInt ( prompt ("Ingresar Numero")); 
+/* let ingresarNumero = parseInt ( prompt ("Ingresar Numero")); 
 
 for (let i = 2; i <= 10; i++){
     let resultado = ingresarNumero * i;
-    alert(ingresarNumero + " " + i + "=" + resultado );
-} 
+    alert(ingresarNumero + " " + i + "=" + resultado ); */
+/* }  */
 
 /* CICLOS CONDICIONALES */
 
-let data = 0;
+/* let data = 0;
 
 do{
     console.log(data);
@@ -34,18 +34,58 @@ do{
     data++;
 } while(data <=10);
 
-console.log ("End of cicle");
+console.log ("End of cicle"); */
 
 
 /* SIMULADOR INTERACTIVO */
 
 /* PROCESO CALCULO DEL IVA */
 
-var precio = 100;
+/* var precio = 100;
 var ivaEnFactura = 23;
 
 var iva = precio * ivaEnFactura/100;
 var precioTotal = precio + iva;
 
-alert(precioTotal);
+alert(precioTotal); */
+
+
+/* CLASE 6 DESAFIO */
+
+/* AGREGANDO ARRAYS */
+
+const libros= ["Orgullo y Prejuicio", "El Perfume", "El color Purpura", "Metamorfosis", "Mujercitas"];
+
+/* alert(libros[3]); */
+
+let total="";
+
+for (let index=0; index < 5; index++){
+    console.log (libros[index]);
+    total= total+ libros [index]
+
+}
+console.log (total);
+
+
+/* AGREGANDO ARRAY AL SIMULADOR INTERACTIVO */
+
+const factura= ["Base sin Iva", "Iva", "Total con Iva"];
+
+factura.push ("descuento");
+
+console.log ("suma de elementos:" + factura.length);
+
+
+let facturaCompleta="";
+
+for (let index= 0; index <3; index++){
+    console.log (factura[index]);
+    facturaCompleta= total + factura[index]
+
+}
+console.log (factura);
+
+
+
 
