73645936fcdfaf0859feb52ad67ba00d7c16e993
Maria Ramirez <maga.ramirez86@gmail.com>
2022-04-26 01:13:14 +0100

    primera entrega

--- index.js
+++ index.js
@@ -41,22 +41,20 @@ console.log ("End of cicle"); */
 
 /* PROCESO CALCULO DEL IVA */
 
-/* var precio = 100;
+var precio = 100;
 var ivaEnFactura = 23;
 
 var iva = precio * ivaEnFactura/100;
 var precioTotal = precio + iva;
 
-alert(precioTotal); */
+alert(precioTotal); 
 
-
-/* CLASE 6 DESAFIO */
 
 /* AGREGANDO ARRAYS */
 
 const libros= ["Orgullo y Prejuicio", "El Perfume", "El color Purpura", "Metamorfosis", "Mujercitas"];
 
-/* alert(libros[3]); */
+alert(libros[3]);
 
 let total="";
 
@@ -87,5 +85,28 @@ for (let index= 0; index <3; index++){
 console.log (factura);
 
 
+/* Ejemplo de Busqueda */
 
+const facturas = [
+    
+    {numero: "00204", montoTotal: 1500},
+    {numero: "00186", montoTotal: 2300},
+    {numero: "00035", montoTotal: 5800},
+
+]
+
+const resultado = facturas.find((el)=> el.numero === "00186")
+const resultado2 = facturas.find((el)=> el.numero === "00204")
+const resultado3 = facturas.find((el)=> el.numero === "00087")
+
+console.log (resultado)
+console.log (resultado2)
+console.log (resultado3)
+
+
+/* EJEMPLO DE FILTER */
+
+const resultadoX = facturas.filter((el)=> el.numero.includes ("00186"))
+const resultadoY = facturas.filter((el)=> el.numero.includes ("00204"))
+const resultadoZ = facturas.filter((el)=> el.montoTotal < 2300)
 
